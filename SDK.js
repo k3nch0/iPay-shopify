403d32642e0eedb1d39d43efb80b9d9b97933a4d
Oto Kenchoshvili <49122364+k3nch0@users.noreply.github.com>
2021-06-15 01:32:43 +0400

    update URLs

--- SDK.js
+++ SDK.js
@@ -21,10 +21,15 @@ class Ipay {
             validate_items: true,
             shop_order_id: this.options.shop_order_id,
             locale: this.options.locale,
-            cart_items: [{items}],
-
+            purchase_units: [{
+                amount: {
+                    currency_code: this.options.currency_code,
+                    value: totalAmount
+                },
+                industry_type: this.options.industry_type
+            }],
+            cart_items: [{items}]
         }
-        console.log(checkoutDetails)
         let token = await this.token;
         const result = await api.checkout(this.options, token.access_token, checkoutDetails);
         if(result.error_message == "Requested token expired/invalid"){
